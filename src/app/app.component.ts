0f5796c5699270af3d91b7207e82349aa1e533c7
Gaëtan Bloch <gaetan.bloch@gmail.com>
2020-04-06 17:22:14 +0200

    init data binding module by possibility to create servers and blueprints

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  serverElements = [];
+  newServerName = '';
+  newServerContent = '';
+
+  onAddServer() {
+    this.serverElements.push({
+      type: 'server',
+      name: this.newServerName,
+      content: this.newServerContent
+    });
+  }
+
+  onAddBlueprint() {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: this.newServerName,
+      content: this.newServerContent
+    });
+  }
 }
