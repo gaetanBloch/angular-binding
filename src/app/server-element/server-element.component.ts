98354f228b2d82c6e819cd96add32d4169a724b4
Gaëtan Bloch <gaetan.bloch@gmail.com>
2020-04-07 19:13:59 +0200

    Checked Lifecycle Hooks in action part 1

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -6,13 +6,19 @@ import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./server-element.component.css'],
   encapsulation: ViewEncapsulation.Emulated // Default value, otherwise None or Native
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements OnInit, OnChanges {
   @Input('srvElement') element: { type: string, name: string, content: string };
 
   constructor() {
+    console.log('Constructor called!');
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('ngOnChanges called!');
+    console.log(changes);
   }
 
   ngOnInit(): void {
+    console.log('ngOnInit called!');
   }
-
 }
