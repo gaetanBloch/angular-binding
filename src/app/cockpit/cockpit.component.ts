8e272fcdda9004ca7b1303ff37ee5f5d8713f8f7
Gaëtan Bloch <gaetan.bloch@gmail.com>
2020-04-07 15:17:53 +0200

    Used Local References in Cockpit Template

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -9,7 +9,7 @@ import { Server } from '../server.model';
 export class CockpitComponent implements OnInit {
   @Output() serverCreated = new EventEmitter<Server>();
   @Output('bpCreated') blueprintCreated = new EventEmitter<Server>();
-  newServerName = '';
+  // newServerName = '';
   newServerContent = '';
 
   constructor() {
@@ -18,11 +18,11 @@ export class CockpitComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  onAddServer() {
-    this.serverCreated.emit(new Server(this.newServerName, this.newServerContent));
+  onAddServer(serverNameInput: HTMLInputElement) {
+    this.serverCreated.emit(new Server(serverNameInput.value, this.newServerContent));
   }
 
-  onAddBlueprint() {
-    this.blueprintCreated.emit(new Server(this.newServerName, this.newServerContent));
+  onAddBlueprint(serverNameInput: HTMLInputElement) {
+    this.blueprintCreated.emit(new Server(serverNameInput.value, this.newServerContent));
   }
 }
